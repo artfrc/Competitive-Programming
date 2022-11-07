4e419aa997eecbf3ba41b8f4e649d507d4d66ae8
artfrc <crfernandesarthur@gmail.com>
2022-11-07 12:06:42 -0300

    strings commit

--- strings/b.cpp
+++ strings/b.cpp
@@ -1,42 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool decres(int a, int b){
 
-	return a > b;
-
-}
 
 int main(void){
 
-	int n;
-	cin >> n;
+	char in[1005];
+	int dia, temp;
+	char sol;
+	while(scanf("%[^\n]s ,%d,%c,%d", in, &dia, &sol, &temp) != EOF){
 
-	vector<int> par;
-	vector<int> impar;
-
-	while(n > 0){
-
-		int x;
-		cin >> x;
-		//cout << x << endl;
-
-		if(x % 2 == 0)
-			par.push_back(x);
-		else
-			impar.push_back(x);
-
-		n--;
+		cout << "OK\n";
 
 	}
-
-	sort(par.begin(),par.end());
-	sort(impar.begin(),impar.end(),decres);
-
-	for(int y : par)
-		cout << y << endl;
-
-	for(int y : impar)
-		cout << y << endl;
 
 }
